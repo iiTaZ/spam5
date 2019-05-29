97b295b3cc1062dcb3e564f23631a8e7daa74f61
iiTaZ <45091481+iiTaZ@users.noreply.github.com>
2019-05-29 21:09:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const client3 = new Discord.Client();
-var prefix = "s";
+var prefix = "1";
 
 client.on('ready', () => {
    console.log(`----------------`);
-   console.log(` by : iiTaZ `);
+   console.log(`Credit Farmm - Script By : Kahrbaa `);
    console.log(`----------------`);
    console.log(`Loadinng`);
    console.log(`Loadinng.`);
@@ -17,13 +16,50 @@ client.on('ready', () => {
 });
 
 
+client.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
 
-client.on('mmessage', message => { // لا تغير شئ عشان ما تخرب الدنيا
+client.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+
+
+client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
 if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 9000000; x++) {
-        message.channel.send(`**HAHAHAHAHAHA**[ " ${x} " ]`)
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -32,26 +68,12 @@ if (message.content === '!spam') {
       }
 });
 
-client2.on('essage', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!s') {
+client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 9000000; x++) {
-        message.channel.send(`**HAHAHAHAHAHA**[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client3.on('essage', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!s') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 9000000; x++) {
-        message.channel.send(`**HAHAHAHAHAHA**[ " ${x} " ]`)
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -69,39 +91,7 @@ client.on('message', message => {
 
   let args = message.content.split(" ").slice(1);
   
-if (command == "s") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
-client2.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-  
-if (command == "s") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
-client3.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-  
-if (command == "s") {
+if (command == "say") {
 if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
    message.channel.sendMessage(args.join("  "))
    message.delete()
@@ -112,4 +102,3 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
-client3.login(process.env.TOKEN3);// لا تغير فيها شيء
