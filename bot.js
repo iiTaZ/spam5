4ecca604d2086014a3574682c23794d53adc3e16
iiTaZ <45091481+iiTaZ@users.noreply.github.com>
2019-05-30 23:54:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,6 +98,22 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
   }
  });
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  
+if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
 
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
