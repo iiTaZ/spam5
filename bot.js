4115696e197cc398cb35caa1111c23044809eabb
iiTaZ <45091481+iiTaZ@users.noreply.github.com>
2019-06-09 07:26:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ if (message.content === '!sp1') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`-match`)
+        message.channel.send(`-math`)
           .then(m => {
             count++;
           })
